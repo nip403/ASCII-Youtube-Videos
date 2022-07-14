605f01897a492ca645cf3de1a4fcf942277e2cb9
Nip403 <41174986+nip403@users.noreply.github.com>
2022-07-14 17:47:15 +0800

    Basic video playing implemented

--- src/Vid2ASCII.py
+++ src/Vid2ASCII.py
@@ -2,6 +2,8 @@ import VidConverter
 import CLI_Player
 import YT_Scraper
 import sys
+import os
+
 
 class Vid2ASCII:
     def __init__(self):
@@ -12,10 +14,17 @@ class Vid2ASCII:
     def play_url(self, url):
         vid = self.yts.scrape(url)
         #convert and play vid (maybe feed vidc into cli?)
+        
+    def play_vid(self, fp):
+        ascii_vid = self.vidc.breakdown(fp)
+        self.cli.play(ascii_vid)
 
 def play(url):
     player = Vid2ASCII()
     player.play_url(url)
 
 if __name__ == '__main__':
-    play(sys.argv[1])
+    #play(sys.argv[1])
+    cdir = os.path.split(__file__)[0] + "\\"
+    b = Vid2ASCII()
+    b.play_vid(cdir + "samplevid.mp4")
