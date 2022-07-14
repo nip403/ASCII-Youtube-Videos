b9dba00b13bf223e130acbf2a4391d763d615e4b
Nip403 <41174986+nip403@users.noreply.github.com>
2022-07-14 17:46:42 +0800

    Very rough video player

--- src/CLI_Player.py
+++ src/CLI_Player.py
@@ -1,7 +1,17 @@
 #play video in cli
 #ideally match frame rate + play audio
 # need to print screen then clear in cmd line
+import sys
+import os
+import time
 
 class Player:
     def __init__(self):
         pass
+    
+    def play(self, frames):
+        for i in frames:
+            os.system("cls")
+            sys.stdout.write(i)
+            time.sleep(1/30)
+            
