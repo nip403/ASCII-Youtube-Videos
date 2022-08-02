b1815240354baec86df001e2213bb67a91195f8a
Nip403 <41174986+nip403@users.noreply.github.com>
2022-08-02 17:30:16 +0800

    Streamlined some stuff

--- src/Vid2ASCII.py
+++ src/Vid2ASCII.py
@@ -1,30 +1,27 @@
 import VidConverter
-import CLI_Player
 import YT_Scraper
+import Utils
+import logging
 import sys
 import os
 
+logger = logging.getLogger("Utils.Vid2ASCII")
 
 class Vid2ASCII:
     def __init__(self):
         self.yts = YT_Scraper.Scraper()
         self.vidc = VidConverter.Converter()
-        self.cli = CLI_Player.Player()
-    
-    def play_url(self, url):
-        vid = self.yts.scrape(url)
-        #convert and play vid (maybe feed vidc into cli?)
         
-    def play_vid(self, fp):
-        ascii_vid = self.vidc.breakdown(fp)
-        self.cli.play(ascii_vid)
-
+    def play_vid(self, args):
+        if args.file is not None:
+            self.vidc.load(args.file, args.playback)
+        
 def play(url):
     player = Vid2ASCII()
     player.play_url(url)
 
 if __name__ == '__main__':
-    #play(sys.argv[1])
-    cdir = os.path.split(__file__)[0] + "\\"
-    b = Vid2ASCII()
-    b.play_vid(cdir + "samplevid.mp4")
+    args = Utils.parser.parse_args()
+    Utils.validate_args(args)
+    player = Vid2ASCII()
+    player.play_vid(args)
