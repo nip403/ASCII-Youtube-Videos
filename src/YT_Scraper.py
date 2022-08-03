f0e3839d68a9452f8523795343625d9ea9eb1559
Nip403 <41174986+nip403@users.noreply.github.com>
2022-08-03 11:50:47 +0800

    Added functionality

--- src/YT_Scraper.py
+++ src/YT_Scraper.py
@@ -1,9 +1,51 @@
-#check if video is 1080p/only get 1080p resolution with dimension 1920x1080
-#potential to stream video instead of downloading?
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+import youtube_dl 
+import os
 
 class Scraper:
     def __init__(self):
-        pass
+        opt = webdriver.ChromeOptions()
+        opt.add_argument("--start-maximized")
+        opt.add_argument("headless")
+
+        chromedriver_autoinstaller.install()
+        self.driver = webdriver.Chrome(options=opt)
+        self.search = r"https://www.youtube.com/results?search_query="
         
-    def scrape(self, url): 
-        return
+    def scrape_video(self, url): 
+        if not "www.youtube.com" in url:
+            self.scrape_video(self.get_search(url))
+            return os.path.dirname(__file__) + "\\temp\\"
+        
+        ydl_opts= {
+            "format": "22",
+            "continue": True,
+            "outtmpl": os.path.dirname(__file__) + "\\temp\\%(uploader)s - %(title)s.%(ext)s",
+   #        'quiet': True,
+          }
+        
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            
+        return os.path.dirname(__file__) + "\\temp\\"
+        
+    def get_search(self, name):
+        self.driver.get(self.search + name.replace(" ", "+"))
+        self.driver.execute_script("var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;")
+        
+        for result in self.driver.find_elements(By.CSS_SELECTOR, ".text-wrapper.style-scope.ytd-video-renderer"):
+            return str(result.find_element(By.CSS_SELECTOR, ".title-and-badge.style-scope.ytd-video-renderer a").get_attribute("href"))
+        
+    def reset(self):
+        tempdir = os.path.dirname(__file__) + "\\temp"
+        
+        for file in os.listdir(tempdir):
+            f = tempdir + "\\" + file
+            
+            if os.path.isfile(f):
+                os.remove(f)
+                
+        os.rmdir(tempdir)
