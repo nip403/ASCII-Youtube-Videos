9cb393ff41e6966808cc34ac84dd056bf53de6ae
Nip403 <41174986+nip403@users.noreply.github.com>
2022-08-12 23:47:05 +0800

    Update Vid2ASCII.py

--- Vid2ASCII.py
+++ Vid2ASCII.py
@@ -1,11 +1,8 @@
 import VidConverter
 import YT_Scraper
 import Utils
-import logging
 import sys
 import os
-
-logger = logging.getLogger("Utils.Vid2ASCII")
 
 class Vid2ASCII:
     def __init__(self):
@@ -22,10 +19,13 @@ class Vid2ASCII:
             file = sorted([os.path.join(tempdir, i) for i in os.listdir(tempdir)], key=os.path.getmtime)[-1]
             self.vidc.load(file, args.playback)
             
-            self.yts.reset()
+            if args.delete:
+                self.yts.reset()
 
 if __name__ == '__main__':
     args = Utils.parser.parse_args()
+    
     Utils.validate_args(args)
+    
     player = Vid2ASCII()
     player.play_vid(args)
